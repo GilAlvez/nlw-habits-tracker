719ad05e785e935f34ca011766ca88ac3259b469
Gil Alves <gildson_af@outlook.com>
2023-01-18 15:24:00 -0300

    fix: Update tailwind config

--- front/tailwind.config.cjs
+++ front/tailwind.config.cjs
@@ -3,9 +3,15 @@ module.exports = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx}",
     "./src/components/**/*.{js,ts,jsx,tsx}",
+    "./src/App.tsx",
+    "./index.html",
   ],
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateRows: {
+        7: "repeat(7, minmax(0, 1fr))",
+      },
+    },
   },
   plugins: [],
 };
