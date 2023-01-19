f24e92836c90d82a5155f286714452bf796ae5a4
Gil Alves <gildson_af@outlook.com>
2023-01-19 20:39:44 -0300

    feat: Add new router in server, toggle habit

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -61,4 +61,84 @@ export const routes = async (server: FastifyInstance) => {
     });
     return { possibleHabits, completedHabits };
   });
+
+  server.patch("/habits/:id/toggle", async (req, res) => {
+    const toggleHabitParams = z.object({
+      id: z.string().uuid(),
+    });
+
+    const { id } = toggleHabitParams.parse(req.params);
+
+    const today = dayjs().startOf("day").toDate();
+
+    // Achar o dia de hoje
+    let day = await prisma.day.findUnique({
+      where: {
+        date: today,
+      },
+    });
+
+    // Caso não ache ele vai criar um na tabela de dias
+    if (!day) {
+      day = await prisma.day.create({
+        data: {
+          date: today,
+        },
+      });
+    }
+
+    // Busca na tabela dayHabits se o usuário ja tinha marcado como completo o habito
+    const dayHabit = await prisma.dayHabit.findUnique({
+      where: {
+        day_id_habit_id: {
+          day_id: day.id,
+          habit_id: id,
+        },
+      },
+    });
+
+    if (dayHabit) {
+      // Uncheck Hábito
+      await prisma.dayHabit.delete({
+        where: {
+          id: dayHabit.id,
+        },
+      });
+    } else {
+      // Check Hábito
+      await prisma.dayHabit.create({
+        data: {
+          day_id: day.id,
+          habit_id: id,
+        },
+      });
+    }
+  });
+
+  server.get("/summary", async (req, res) => {
+    const summary = await prisma.$queryRaw`
+      SELECT 
+        D.id,
+        D.date,
+        (
+          SELECT 
+            cast(count(*) as float)
+          FROM day_habits DH
+          WHERE DH.day_id = D.id
+        ) as completed
+        (
+          SELECT
+            cast(count(*) as float)
+          FROM habit_week_days HWD
+          JOIN habits H
+            ON H.id = HWD.habit_id
+          WHERE 
+            HWD.week_day = cast(strftime('%w', D.date/1000.0, 'unixepoch') as int)
+            AND H.created_at <= D.date
+        ) as amount
+      FROM days D
+    `;
+
+    return summary;
+  });
 };
