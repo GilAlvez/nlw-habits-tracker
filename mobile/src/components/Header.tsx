947895646a4c180f4bd246c887b0ed3f11275dd3
Gil Alves <gildson_af@outlook.com>
2023-01-19 20:35:52 -0300

    feat: Add new componente, header

--- mobile/src/components/Header.tsx
+++ mobile/src/components/Header.tsx
@@ -2,14 +2,17 @@ import { TouchableOpacity, View, Text } from "react-native";
 import Logo from "../assets/logo.svg";
 import { Feather } from "@expo/vector-icons";
 import colors from "tailwindcss/colors";
+import { useNavigation } from "@react-navigation/native";
 
 export const Header = () => {
+  const { navigate } = useNavigation();
   return (
     <View className="flex-row items-center justify-between">
       <Logo />
       <TouchableOpacity
         className="flex-row items-center px-3 py-2 border rounded-lg gap-x-2 border-violet-500"
         activeOpacity={0.6}
+        onPress={() => navigate("new")}
       >
         <Feather name="plus" color={colors.violet[500]} size={22} />
         <Text className="text-base font-semibold text-white">Novo</Text>
