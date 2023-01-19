292d758b1a8e4d2fc6d467995b946acf43729125
Gil Alves <gildson_af@outlook.com>
2023-01-19 20:35:31 -0300

    feat: Add new componente, habit day

--- mobile/src/components/HabitDay.tsx
+++ mobile/src/components/HabitDay.tsx
@@ -1,6 +1,11 @@
-import { Dimensions, TouchableOpacity, View } from "react-native";
+import {
+  Dimensions,
+  TouchableOpacity,
+  TouchableOpacityProps,
+  View,
+} from "react-native";
 
-interface HabitDayProps {
+interface HabitDayProps extends TouchableOpacityProps {
   active?: boolean;
 }
 
@@ -10,16 +15,15 @@ export const DAY_MARGIN_BETWEEN = 8;
 export const DAY_SIZE =
   Dimensions.get("screen").width / WEEK_DAYS - (SCREEN_HORIZONTAL_PADDING + 5);
 
-export const HabitDay = ({ active }: HabitDayProps) => {
+export const HabitDay = ({ active, ...rest }: HabitDayProps) => {
   return (
-    <View>
-      <TouchableOpacity
-        className={`m-1 border-2 rounded-lg bg-zinc-900 border-zinc-800 ${
-          active ? "opacity-100" : "opacity-70"
-        }`}
-        style={{ width: DAY_SIZE, height: DAY_SIZE }}
-        activeOpacity={0.6}
-      />
-    </View>
+    <TouchableOpacity
+      className={`m-1 border-2 rounded-lg bg-zinc-900 border-zinc-800 ${
+        active ? "opacity-100" : "opacity-70"
+      }`}
+      style={{ width: DAY_SIZE, height: DAY_SIZE }}
+      activeOpacity={0.6}
+      {...rest}
+    />
   );
 };
