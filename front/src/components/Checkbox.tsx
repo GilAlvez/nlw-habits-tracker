bec7bd64cbe235baf03a26438ab1590ea49d75d3
Gil Alves <gildson_af@outlook.com>
2023-01-21 22:10:30 +0100

    feat: Add Checkbox disabled state and transition

--- front/src/components/Checkbox.tsx
+++ front/src/components/Checkbox.tsx
@@ -5,17 +5,24 @@ interface CheckboxProps {
   title: string;
   onChange?: (checked: UICheckbox.CheckedState) => void;
   checked?: UICheckbox.CheckedState;
+  disabled?: boolean;
 }
 
-export const Checkbox = ({ title, checked, onChange }: CheckboxProps) => {
+export const Checkbox = ({
+  title,
+  checked,
+  onChange,
+  disabled,
+}: CheckboxProps) => {
   return (
     <div className="mt-2 flex flex-col gap-3 ">
       <UICheckbox.Root
         className="flex items-center gap-3 group"
         checked={checked}
         onCheckedChange={onChange}
+        disabled={disabled}
       >
-        <div className="h-7 w-7 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-600">
+        <div className="h-7 w-7 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-600 transition-all">
           <UICheckbox.Indicator>
             <Check size={20} className="text-white" />
           </UICheckbox.Indicator>
